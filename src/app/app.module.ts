a90fdaea9aa46a34ddac96b9ba01fd6448accac8
victorjpe <victor.jose@logicline.de>
2017-06-15 11:51:39 +0530

    Initial project setup for voting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {  AppRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutes,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
